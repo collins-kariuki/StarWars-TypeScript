e983f2290c32809f0f911fe684e22919c1c3f5d3
Collins Kariuki <collins_kuria@hotmail.com>
2021-08-03 04:33:46 +0300

    Added Search component to handle search query

---
+++ src/components/Search.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import People from "./People";
+import { useQuery } from "@apollo/client";
+import { PERSON_QUERY } from "./Queries";
+import { useParams } from "react-router-dom";
+
+const Search: React.FC = () => {
+  const { name }: { name: string } = useParams();
+  console.log(name);
+  const usePeople = () => {
+    const { loading, error, data } = useQuery(PERSON_QUERY, {
+      variables: { name },
+    });
+    return { loading, error, data };
+  };
+
+  return <People data={usePeople} currentPage={1} />;
+};
+
+export default Search;
